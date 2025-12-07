5f7d0d4f0f5cb40054cae761ccd19a9e6e73d06c
Vladislavs Viktorovs <vv.viktorovs@gmail.com>
2025-12-07 14:42:49 +0100

    7.2

--- tasks/_build/default/lib/task_7.ml
+++ tasks/_build/default/lib/task_7.ml
@@ -1,6 +1,7 @@
 module IntSet = Set.Make(Int)
 
 let string_of_list l = List.fold_left (fun acc a -> acc^(string_of_int a)^", ") "" l
+let string_of_tuple_list l = List.fold_left (fun acc (n, t) -> Printf.sprintf "%s (%d, %d);" acc n t) "" l
 let string_of_set set = let l = IntSet.to_list set in string_of_list l
 
 let divider = '^'
@@ -24,12 +25,12 @@ let get_sum_divisions file_name =
     if IntSet.is_empty x then
       aux splits curr_beam_ids xs
     else
-      let () = Printf.printf "beams: %s dividers: %s\n" (string_of_list curr_beam_ids) (string_of_set x) in
+      (* let () = Printf.printf "beams: %s dividers: %s\n" (string_of_list curr_beam_ids) (string_of_set x) in *)
       let rec get_new_beam_ids new_splits i res = function
       | [] -> (res, new_splits)
       | b::bs ->
         if IntSet.mem b x then
-          let () = print_string "a\n" in
+          (* let () = print_string "a\n" in *)
           get_new_beam_ids (new_splits+1) (i+1) ((b-1)::(b+1)::res) bs
         else
           (* let () = print_string "b\n" in *)
@@ -37,3 +38,35 @@ let get_sum_divisions file_name =
     in let new_beams, new_splits = get_new_beam_ids 0 0 [] (IntSet.to_list (IntSet.of_list curr_beam_ids)) in
   aux (splits+new_splits) new_beams xs 
 in aux 0 [start_id] (List.rev divider_ids)
+
+let merge_duplicates l = let sorted = List.sort (fun (n1, _) (n2, _) -> compare n1 n2) l in
+  let rec aux (prev_n, prev_times) res = function
+  | [] -> ((prev_n, prev_times)::res)
+  | (n, t)::xs ->
+    if n = prev_n then aux (prev_n, t+prev_times) res xs
+    else aux (n, t) ((prev_n, prev_times)::res) xs
+  in let n1, t1 = List.nth sorted 0 in aux (n1, t1) [] (List.drop 1 sorted)
+
+let get_sum_timelines file_name =
+  let lines = Utils.read_lines file_name in
+  let start_id = get_start_id (List.nth lines 0) in
+  let divider_ids = get_divider_ids lines in
+  let rec aux curr_beam_ids curr_dividers = match curr_dividers with
+  | [] -> List.fold_left (fun acc (_, a) -> acc + a) 0 curr_beam_ids
+  | x::xs -> 
+    if IntSet.is_empty x then
+      aux curr_beam_ids xs
+    else
+      (* let () = Printf.printf "beams: %s dividers: %s\n" (string_of_tuple_list curr_beam_ids) (string_of_set x) in *)
+      let rec get_new_beam_ids res = function
+      | [] -> res
+      | (b, num_timelines)::bs ->
+        if IntSet.mem b x then
+          (* let () = print_string "a\n" in *)
+          get_new_beam_ids ((b-1, (num_timelines))::(b+1, (num_timelines))::res) bs
+        else
+          (* let () = print_string "b\n" in *)
+          get_new_beam_ids ((b, num_timelines)::res) bs
+    in let new_beams = get_new_beam_ids [] (merge_duplicates curr_beam_ids) in
+  aux new_beams xs 
+in aux [(start_id, 1)] (List.rev divider_ids)
